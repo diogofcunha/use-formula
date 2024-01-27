fde7db56feb606ec191fab70fab89328113719bf
Diogo Cunha <diogofncunha@gmail.com>
2024-01-27 16:02:30 +0000

    feat: 🎸 integrated formula store into hook

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { createFormulaStore } from "formula-store";
+import { Grid } from "./types";
 
 export default function useFormula() {
-  const s = useState(0);
+  const [grid, setGrid] = useState<Grid>([[]]);
+  const cellIdxById = useRef(new Map<string, [number, number]>());
 
-  return s;
+  useRef(
+    createFormulaStore({
+      onChange: (updates) => {
+        const newGrid = grid.slice();
+
+        for (const { id, value } of updates) {
+          const idx = cellIdxById.current.get(id);
+
+          if (!idx) {
+            throw new Error(`Failed to get cell with id ${id}`);
+          }
+
+          newGrid[idx[0]] = newGrid[idx[0]].slice();
+
+          const cell = newGrid[(idx[0], idx[1])];
+
+          if (cell === undefined) {
+            throw new Error(`Failed to get cell at (${idx[0]},${idx[1]})`);
+          }
+
+          newGrid[idx[0]][idx[1]] = value as number;
+        }
+
+        setGrid(newGrid);
+      },
+    })
+  );
+
+  return grid;
 }
