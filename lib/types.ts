3fe4c190fed8540b1722c8aa593ee17b09df4789
Diogo Cunha <diogofncunha@gmail.com>
2024-01-27 16:06:49 +0000

    feat: 🎸 added result type for use formula

--- lib/types.ts
+++ lib/types.ts
@@ -1,2 +1,13 @@
 export type Cell = number;
 export type Grid = Cell[][];
+
+export interface UpdateCellPayload {
+  rowIdx: number;
+  columnIdx: number;
+  value: Cell;
+}
+
+export interface UseFormula {
+  grid: Grid;
+  updateCellValue: (update: UpdateCellPayload) => void;
+}
