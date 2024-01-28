538f489c45d0eb7cb8cd3a294bb13d2d7fa0c224
Diogo Cunha <diogofncunha@gmail.com>
2024-01-28 21:47:22 +0000

    test: 💍 added basic tests for formula parsing

--- lib/__tests__/parse-cell.test.ts
+++ lib/__tests__/parse-cell.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from "vitest";
-import { parseCell } from "../parse-cell";
+import { parseCell, Formula } from "../parse-cell";
 
 describe("parseCell", () => {
   test("should return a number if a number is provided", () => {
@@ -15,5 +15,29 @@ describe("parseCell", () => {
   test("should return a string if a simple string is provided", () => {
     expect(parseCell("Hello")).toEqual("Hello");
     expect(parseCell("Hello 234")).toEqual("Hello 234");
+  });
+
+  test("should handle sum formulas", () => {
+    const { dependencies, calculate } = parseCell("=A1+A2+B1") as Formula;
+    expect(dependencies).toEqual(["0-0", "1-0", "0-1"]);
+    expect(calculate(1, 2, 3)).toEqual(6);
+  });
+
+  test("should handle sum formulas with repeated cells", () => {
+    const { dependencies, calculate } = parseCell("=A1+A2+B1+A1") as Formula;
+    expect(dependencies).toEqual(["0-0", "1-0", "0-1"]);
+    expect(calculate(1, 2, 3)).toEqual(7);
+  });
+
+  test("should handle multiplication formulas", () => {
+    const { dependencies, calculate } = parseCell("=A1*A2*B1") as Formula;
+    expect(dependencies).toEqual(["0-0", "1-0", "0-1"]);
+    expect(calculate(1, 2, 4)).toEqual(8);
+  });
+
+  test("should handle multiplication formulas with repeated cells", () => {
+    const { dependencies, calculate } = parseCell("=A1*A2*B1*A2") as Formula;
+    expect(dependencies).toEqual(["0-0", "1-0", "0-1"]);
+    expect(calculate(1, 2, 4)).toEqual(16);
   });
 });
