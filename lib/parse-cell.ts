5f724870a15e0bdcf375b8b2124cf2857ed417f3
Diogo Cunha <diogofncunha@gmail.com>
2024-01-28 21:47:08 +0000

    feat: 🎸 added complex formula parse to cell parser

--- lib/parse-cell.ts
+++ lib/parse-cell.ts
@@ -1,6 +1,14 @@
 import { Cell } from "./types";
+import { getIdxKey } from "./utils";
+import { evaluate } from "mathjs";
 
-export function parseCell(cell: Cell): number | string {
+export interface Formula {
+  dependencies: string[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  calculate: (...dependencies: any[]) => any;
+}
+
+export function parseCell(cell: Cell): number | string | Formula {
   if (typeof cell === "number") {
     return cell;
   }
@@ -11,7 +19,52 @@ export function parseCell(cell: Cell): number | string {
     }
 
     if (cell.startsWith("=")) {
-      //
+      let formula = cell.slice(1);
+      const dependencyRegex = /[A-Z]+[1-9][0-9]*/g;
+      const matches = cell.match(dependencyRegex) || [];
+
+      const dependencies: string[] = [];
+      const dependencyTrack = new Set<string>();
+
+      for (const match of matches) {
+        const columnPart = match.match(/[A-Z]+/)?.[0];
+        const rowPart = match.match(/[1-9][0-9]*/)?.[0];
+
+        if (columnPart === undefined || rowPart === undefined) {
+          return "#ERROR";
+        }
+
+        let column = 0;
+        for (let i = 0; i < columnPart.length; i++) {
+          column =
+            column * 26 + (columnPart.charCodeAt(i) - "A".charCodeAt(0) + 1);
+        }
+        column -= 1;
+        const row = parseInt(rowPart) - 1;
+
+        if (dependencyTrack.has(getIdxKey(row, column))) {
+          continue;
+        }
+
+        dependencyTrack.add(getIdxKey(row, column));
+        dependencies.push(getIdxKey(row, column));
+        const replaceRegex = new RegExp(match, "g");
+        formula = formula.replace(replaceRegex, getIdxKey(row, column));
+      }
+
+      return {
+        dependencies,
+        calculate(...dependencyValues) {
+          let toEvaluate = formula;
+
+          dependencyValues.forEach((d, i) => {
+            const replaceRegex = new RegExp(dependencies[i], "g");
+            toEvaluate = toEvaluate.replace(replaceRegex, d);
+          });
+
+          return evaluate(toEvaluate);
+        },
+      };
     } else {
       return cell;
     }
