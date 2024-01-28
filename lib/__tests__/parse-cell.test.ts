42bc989b2d6789fdce5adeb4061862d4592a249e
Diogo Cunha <diogofncunha@gmail.com>
2024-01-28 20:58:26 +0000

    test: 💍 added test for parse cell numbers

---
+++ lib/__tests__/parse-cell.test.ts
@@ -0,0 +1,9 @@
+import { expect, test, describe } from "vitest";
+import { parseCell } from "../parse-cell";
+
+describe("parseCell", () => {
+  test("should return a number if a number is provided", () => {
+    expect(parseCell(123)).toEqual(123);
+    expect(parseCell(123.23)).toEqual(123.23);
+  });
+});
