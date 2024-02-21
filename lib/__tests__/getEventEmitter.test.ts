6d7197b41d3613fd166c34d870f52deb50ec3f05
Diogo Cunha <diogofncunha@gmail.com>
2024-02-21 21:52:07 +0000

    test: 💍 added tests for getEventEmitter

---
+++ lib/__tests__/getEventEmitter.test.ts
@@ -0,0 +1,31 @@
+import { expect, test, describe } from "vitest";
+import { CellCoordinates, getEventEmitter } from "../getEventEmitter";
+
+describe("getEventEmitter", () => {
+  test("should emit and subscribe", () => {
+    const { emitEvent, subscribe } = getEventEmitter();
+
+    const calls: CellCoordinates[] = [];
+
+    const cb = (c: CellCoordinates) => {
+      calls.push(c);
+    };
+
+    emitEvent({ rowIdx: 0, columnIdx: 1 });
+
+    const unsubscribe = subscribe(cb);
+
+    emitEvent({ rowIdx: 2, columnIdx: 1 });
+    emitEvent({ rowIdx: 3, columnIdx: 1 });
+
+    unsubscribe();
+
+    emitEvent({ rowIdx: 4, columnIdx: 1 });
+    emitEvent({ rowIdx: 5, columnIdx: 1 });
+
+    expect(calls).toEqual([
+      { rowIdx: 2, columnIdx: 1 },
+      { rowIdx: 3, columnIdx: 1 },
+    ]);
+  });
+});
