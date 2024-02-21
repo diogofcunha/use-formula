8a20569e7b4a2bcafea9ecc4407a00566e82391d
Diogo Cunha <diogofncunha@gmail.com>
2024-02-21 20:48:30 +0000

    refactor: 💡 started to use provider un app

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
 import "./App.css";
-import useFormula from "../";
-import Cell from "./components/Cell";
-import { useRenderCounter } from "./hooks/useRenderCounter";
+import { FormulaProvider } from "../";
+import Grid from "./components/Grid";
 
 function App() {
-  const { grid, updateCellValues } = useFormula([
-    [0, 1, 2, 3, 4],
-    [0, "=A1+B1+C1+C3", 2, 3, 4],
-    [0, 1, 2, 3, 4],
-  ]);
-
-  const renderCount = useRenderCounter();
-
   return (
-    <>
-      <div>Rendered {renderCount}</div>
-      <h1>Simple example</h1>
-      <table>
-        <tbody>
-          {grid.map((row, rowIdx) => {
-            return (
-              <tr key={rowIdx}>
-                {row.map((column, columnIdx) => {
-                  return (
-                    <Cell
-                      key={`${rowIdx}-${columnIdx}`}
-                      columnIdx={columnIdx}
-                      rowIdx={rowIdx}
-                      value={column}
-                      onChange={updateCellValues}
-                    />
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+    <FormulaProvider
+      initialGrid={[
+        [0, 1, 2, 3, 4],
+        [0, "=A1+B1+C1+C3", 2, 3, 4],
+        [0, 1, 2, 3, 4],
+      ]}
+    >
+      <Grid />
+    </FormulaProvider>
   );
 }
 
