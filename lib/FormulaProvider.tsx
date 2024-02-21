4c015094bdc34f2fba49df89516a877c8fcfe189
Diogo Cunha <diogofncunha@gmail.com>
2024-02-21 20:47:33 +0000

    refactor: 💡 moved hook logic to provider

---
+++ lib/FormulaProvider.tsx
@@ -0,0 +1,150 @@
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { Formula, Grid, UseFormula } from "./types";
+import { createFormulaStore } from "formula-store";
+import { parseCell } from "./parse-cell";
+import { v4 } from "uuid";
+import { getIdxKey } from "./utils";
+import { FormulaField } from "formula-store/lib/types";
+
+export const FormulaContext = React.createContext<UseFormula>({
+  grid: [],
+  updateCellValues: () => {},
+});
+
+export function FormulaProvider({
+  initialGrid,
+  children,
+}: PropsWithChildren<{ initialGrid: Grid }>) {
+  const [grid, setGrid] = useState<Grid>(initialGrid);
+  const cellIdxById = useRef(new Map<string, [number, number]>());
+  const cellIdByIdx = useRef(new Map<string, string>());
+
+  const mapDependencies = (formula: Formula) =>
+    formula.dependencies.map((d) => {
+      return cellIdByIdx.current.get(d)!;
+    });
+
+  const store = useRef(
+    createFormulaStore({
+      onChange: (updates) => {
+        setGrid((g) => {
+          const newGrid = g.slice();
+
+          for (const { id, value } of updates) {
+            const idx = cellIdxById.current.get(id);
+
+            if (!idx) {
+              throw new Error(`Failed to get cell with id ${id}`);
+            }
+
+            newGrid[idx[0]] = newGrid[idx[0]].slice();
+
+            const cell = newGrid[idx[0]][idx[1]];
+
+            if (cell === undefined) {
+              throw new Error(`Failed to get cell at (${idx[0]},${idx[1]})`);
+            }
+
+            newGrid[idx[0]][idx[1]] = value as number;
+          }
+
+          return newGrid;
+        });
+      },
+    })
+  );
+
+  useEffect(() => {
+    const formulas: Array<Formula & { id: string }> = [];
+
+    for (let rowIdx = 0; rowIdx < initialGrid.length; rowIdx++) {
+      const column = initialGrid[rowIdx];
+
+      for (let columnIdx = 0; columnIdx < column.length; columnIdx++) {
+        const cell = parseCell(column[columnIdx]);
+        const id = v4();
+
+        let value: string | number = "";
+
+        if (typeof cell === "object") {
+          value = "";
+          formulas.push({ ...cell, id });
+        } else {
+          value = cell;
+        }
+
+        store.current.addField({
+          dependencies: [],
+          id,
+          value,
+        });
+
+        cellIdxById.current.set(id, [rowIdx, columnIdx]);
+        cellIdByIdx.current.set(getIdxKey(rowIdx, columnIdx), id);
+      }
+    }
+
+    for (const f of formulas) {
+      store.current.editField({
+        dependencies: mapDependencies(f),
+        calculate: f.calculate,
+        id: f.id,
+        value: "",
+      });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <FormulaContext.Provider
+      value={{
+        grid,
+        updateCellValues: (updates) => {
+          const cells = updates.map((u) => {
+            const id = cellIdByIdx.current.get(
+              getIdxKey(u.rowIdx, u.columnIdx)
+            );
+
+            if (id === undefined) {
+              throw new Error(`Cell (${u.rowIdx},${u.columnIdx}) not found.`);
+            }
+
+            return { cell: parseCell(u.value), id };
+          });
+
+          const simpleUpdates: Array<{ value: unknown; id: string }> = [];
+          const fullFieldUpdates: Array<FormulaField<unknown>> = [];
+
+          for (const { cell, id } of cells) {
+            if (typeof cell === "object") {
+              fullFieldUpdates.push({
+                calculate: cell.calculate,
+                dependencies: mapDependencies(cell),
+                id,
+                value: "",
+              });
+            } else {
+              simpleUpdates.push({ value: cell, id });
+            }
+          }
+
+          if (simpleUpdates.length) {
+            store.current.updateFieldsValue(simpleUpdates);
+          }
+
+          for (const f of fullFieldUpdates) {
+            store.current.editField({
+              dependencies: f.dependencies,
+              calculate: f.calculate,
+              id: f.id,
+              value: "",
+            });
+          }
+        },
+      }}
+    >
+      {children}
+    </FormulaContext.Provider>
+  );
+}
