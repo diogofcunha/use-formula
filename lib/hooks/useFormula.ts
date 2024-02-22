bcd2e7a43fc88b8aec445006abcbfb91d60f5285
Diogo Cunha <diogofncunha@gmail.com>
2024-02-22 22:13:37 +0000

    feat: 🎸 added watch cell back

--- lib/hooks/useFormula.ts
+++ lib/hooks/useFormula.ts
@@ -3,10 +3,11 @@ import { FormulaContextValue } from "../types";
 import { FormulaContext } from "../FormulaProvider";
 
 export function useFormula(): FormulaContextValue {
-  const { getGrid, updateCellValues } = useContext(FormulaContext);
+  const { getGrid, updateCellValues, watchCell } = useContext(FormulaContext);
 
   return {
     getGrid,
     updateCellValues,
+    watchCell,
   };
 }
